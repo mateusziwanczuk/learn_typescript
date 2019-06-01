86441925a2b084b1fb90d388d8c5990056b2e360
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-06-01 20:38:37 +0200

    Add hooks and interface/types. Displaying input value in list after submit.

--- ts_basics/src/index.tsx
+++ ts_basics/src/index.tsx
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom';
 
 type FormElem = React.FormEvent<HTMLFormElement>
  
+interface ITodo { // ITodo = Interface Todo
+    text: string,
+    complete: boolean
+}
+
+    /*  
+        interface ITodo2 extends ITodo {
+            tags: string
+        }
+            // impossible with 'type' 
+    */
+
 export default function App(): JSX.Element {
 
     const [value, setValue] = useState<string>("")
+    const [todos, setTodos] = useState<ITodo[]>([])
 
     const handleSubmit = (e:FormElem):void => {
         e.preventDefault()
+        addTodo(value)
         setValue('')
+    }
+
+    const addTodo = (text:string) => {
+        const newTodos: ITodo[] = [...todos, { 
+            text, 
+            complete: false 
+        }]
+        setTodos(newTodos)
     }
 
     return (
@@ -24,6 +46,15 @@ export default function App(): JSX.Element {
                 />
                 <button type="submit">Add Todo</button>
             </form>
+            <section>
+                {todos.map((todo:ITodo, index:number) => {
+                    return (
+                        <li key={index}>
+                            {todo.text}
+                        </li>
+                    )
+                })}
+            </section>
         </Fragment>
     )
 }
