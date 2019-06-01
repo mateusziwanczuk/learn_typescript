df5d6777667e32f6a08f33d6aac2b07ad6afb5b7
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-06-01 16:13:36 +0200

    Babel configuration, fix package.json scripts.

--- ts_basics/webpack.config.js
+++ ts_basics/webpack.config.js
@@ -1,9 +1,29 @@
+const path = require('path')
+
+const rules = [
+    {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+    }
+]
+
 module.exports = {
     target: 'web',
     mode: 'development',
-    entry: './src/index/tsx'
+    entry: './src/index.tsx',
     output: {
-        path: null,
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
+    },
+    module: { 
+        rules 
+    },
+    resolve: {
+        extentions:['.ts', '.tsx', '.js']
+    },
+    devServer: {
+        contentBase: './',
+        port: 5000
     }
 }
