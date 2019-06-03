29737b68180c3dccb405c7043cc546423673af76
mateuszmatzh <iwanczukmateusz@gmail.com>
2019-06-03 11:16:39 +0200

    Iterfaces - TypeScript examples.

---
+++ ts_basics/interfaces.js
@@ -0,0 +1,11 @@
+// function showTodo(todo: { title: string, text: string }) {
+//     console.log(todo.title + ' ' + todo.text)
+// }
+function showTodo(todo) {
+    console.log(todo.title + " " + todo.text);
+}
+var myTodo = {
+    title: 'trash',
+    text: 'take out trash'
+};
+showTodo(myTodo);
